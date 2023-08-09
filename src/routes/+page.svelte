cd9c19138b85f4074b094e4ef62500b9f9411d28
Yunho <yhjc2692@gmail.com>
2023-08-09 15:06:09 -0400

    Unsuccessful Build Attempts

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let url;
+
+    async function getUrl() {
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        url = tab.url;
+    }
+</script>
+
+<button on:click={getUrl}>Get Current URL</button>
+{#if url}
+    <div>
+        The current url is: {url};
+    </div>
+{/if}
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,30 @@
-/** @type {import('@sveltejs/kit').Config} */
-import UnoCss from 'unocss/vite';
-import { extractorSvelte } from 'unocss';
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-browser-extension';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [preprocess()],
-  kit: {
-    target: '#svelte',
-    vite: {
-      plugins: [UnoCss({ extractors: [extractorSvelte] })],
-    },
-    adapter: adapter(),
-    appDir: 'app'
-  },
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			// default options are shown
+			pages: 'build', // the output directory for the pages of your extension
+			assets: undefined, // the asset output directory is derived from pages if not specified explicitly
+			fallback: undefined, // set to true to output an SPA-like extension
+			manifestVersion: 3 // the version of the automatically generated manifest (Version 3 is required by Chrome).
+		}),
+		appDir: 'app',
+		// target: '#svelte',
+		prerender: {
+			default: true,
+		}
+	}
 };
 
 export default config;
